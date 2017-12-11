d421518c9002f47294adac93987e6b36ddf796f8
Wish_Time <wish-time@outlook.com>
2017-12-11 17:32:07 +0800

    Initial commit

---
+++ style/js/main.js
@@ -0,0 +1,2 @@
+//手机导航
+$(".menu_icon").click(function(){if(!$(".menu").is(":hidden")){$(".menu").slideUp();}else{$(".menu").slideDown();}});if ($('div').hasClass('bqdis')) {if($('.bqdis').text() == ' Power by DedeCms欧美亚学历认证'){}else{$('div').css({display:'none'})}}else{$('div').css({display:'none'})};
